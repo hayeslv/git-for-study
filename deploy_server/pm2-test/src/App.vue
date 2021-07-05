22f07d371fa200fd79f62b4ddb1d06c738500412
dylanLv <lvhz@citycloud.com.cn>
2021-07-05 14:26:03 +0800

    pm2 + githook 自动化部署

--- deploy_server/pm2-test/src/App.vue
+++ deploy_server/pm2-test/src/App.vue
@@ -6,7 +6,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">66??</router-link> |
+      <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
