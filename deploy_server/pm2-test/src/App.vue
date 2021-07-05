0fb3a94e78f380783518d70ef9933d4d35e590f0
dylanLv <lvhz@citycloud.com.cn>
2021-07-05 10:06:24 +0800

    1

--- deploy_server/pm2-test/src/App.vue
+++ deploy_server/pm2-test/src/App.vue
@@ -6,7 +6,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home444</router-link> |
+      <router-link to="/">Home555</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
