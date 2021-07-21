10309149b4f93abf7826310a84358bfa217d33c6
lvhaizhou <lvhaizhou2012@gmail.com>
2021-07-21 21:43:23 +0800

    状态机

---
+++ frameworkPerson/基础点/day3/StateMachine.ts
@@ -0,0 +1,18 @@
+// enum States {
+//   Start,
+//   DragStart,
+//   Moving,
+//   Stoped,
+//   Selected
+// }
+
+// export enum Actions {
+//   AUTO,
+//   EvtDragStart,
+//   EvtDrag,
+//   EvtDragEnd
+// }
+
+class StateMachine1<S extends number, A extends number> {
+  s : S
+}
