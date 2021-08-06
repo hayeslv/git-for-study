0d144a81e1a15556dfe0c5e31a1f4bb14a8431a4
lvhaizhou <lvhaizhou2012@gmail.com>
2021-08-06 11:37:06 +0800

    monaco使用

--- editor/monaco-editor/src/main.ts
+++ editor/monaco-editor/src/main.ts
@@ -1,6 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
-createApp(App).use(store).use(router).mount('#app')
+import ElementUI from 'element-plus';
+
+
+const app = createApp(App)
+  .use(store)
+  .use(router)
+  .use(ElementUI)
+  .mount('#app');
